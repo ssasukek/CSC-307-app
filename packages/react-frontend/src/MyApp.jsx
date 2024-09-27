b06e6a2bce73e48c6b89d6f5bb67c5d991af0ab7
ssasukek <kevinlu3234@gmail.com>
2024-09-26 18:50:52 -0700

    problem 7 commit

--- packages/react-frontend/src/MyApp.jsx
+++ packages/react-frontend/src/MyApp.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import Table from "./Table";
+import Form from "./Form";
 
 function MyApp() {
   const [characters, setCharacters] = useState([
@@ -34,6 +35,7 @@ function MyApp() {
         characterData={characters} 
         removeCharacter={removeOneCharacter}
       />
+      <Form />
     </div>
   );
 }
