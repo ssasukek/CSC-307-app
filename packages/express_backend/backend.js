4c5afd30108097a3a3612955194e65c88117145d
ssasukek <kevinlu3234@gmail.com>
2024-10-09 19:24:58 -0700

    lab3 - 3

--- packages/express_backend/backend.js
+++ packages/express_backend/backend.js
@@ -1,9 +1,11 @@
 import express from "express";
+import cors from "cors";
 
 const app = express();
 const port = 8000;
 
 app.use(express.json());
+app.use(cors());
 
 const users = {
   users_list: [
--- packages/react-frontend/src/MyApp.jsx
+++ packages/react-frontend/src/MyApp.jsx
@@ -1,10 +1,34 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Table from "./Table";
 import Form from "./Form";
 
 function MyApp() {
   const [characters, setCharacters] = useState([]);
   <Form handleSubmit={updateList} />;
+
+  function postUser(person) {
+    const promise = fetch("Http://localhost:8000/users", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(person),
+    });
+
+    return promise;
+  }
+
+  function fetchUsers(){
+    const promise = fetch("http://localhost:8000/users");
+    return promise;
+  }
+
+  useEffect(() => {
+    fetchUsers()
+            .then((res) => res.json())
+            .then((json) => setCharacters(json["users_list"]))
+            .catch((error) => { console.log(error); });
+  }, [] );
 
 
   function removeOneCharacter(index) {
@@ -15,7 +39,11 @@ function MyApp() {
   }
 
   function updateList(person) {
-    setCharacters([...characters, person]);
+    postUser(person)
+      .then(() => setCharacters([...characters, person]))
+      .catch((error) => {
+        console.log(error);
+      });
   }
 
   return (
@@ -24,5 +52,6 @@ function MyApp() {
       <Form handleSubmit={updateList} />
     </div>
   );
+
 }
 export default MyApp;
